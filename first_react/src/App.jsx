b42172e76962aa6c84698f7f6528343eaeb67295
Mostafa002 <mostafamadadi.1382@gmail.com>
2026-04-28 21:55:04 +0430

    creating the hooks

--- first_react/src/App.jsx
+++ first_react/src/App.jsx
@@ -6,32 +6,34 @@ import './App.css'
 import Card from './card'
 import info from './info'
 import info2 from './info2'
-function creatcard(cont) {
 
-  return <Card
-    key={cont.id}
-    img={cont.img}
-    name={cont.name}
-    email={cont.email}
-  />
-}
 function App() {
-  return (
-    <div style={{ display: "flex", flexWrap: "wrap", gap: "5px" }}>
-      {info.map(creatcard)}
-      {info2.map(creatcard)}
-      {info2.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
-      {info.map(creatcard)}
+  const [conatact, setcontact] = useState({
+    fname: "",
+    last: "",
+    pass: ""
+  })
 
+  function handle(event) {
+    const { name, value } = event.target;
+    setcontact((pre) => {
+      return (
+        {
+          ...pre,
+          [name]: value
+        }
+      )
+    })
+  }
+
+  return (
+    <div>
+      <h1>hello {conatact.fname}  {conatact.last}</h1>
+      <p>{conatact.pass}</p>
+      <input onChange={handle} type="text" placeholder='name' name='fname' />
+      <input onChange={handle} type="text" placeholder='last' name='last' />
+      <input onChange={handle} type="password" placeholder="password" name='pass' />
     </div>
-  )
+  );
 }
-export default App
+export default App;
